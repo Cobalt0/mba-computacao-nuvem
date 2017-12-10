b825a52b1effbb23ee3203b1bde8d918ad6df9c6
thiago.assis@claro.com.br <thiago_cobalt@hotmail.com>
2017-12-10 18:52:52 -0200

    Aplicação em nuvem

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\cobalto\desktop\mba\desenv~1\projeto\git\python~1\appeng~1\standard\django\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
